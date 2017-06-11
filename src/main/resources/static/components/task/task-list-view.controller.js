9747f89ec851e6eacb36a652458637af957bb92b
onlythenaive <ilya.gubarev@gmail.com>
2017-06-11 23:01:27 +0300

    task list view publication fixed

--- src/main/resources/static/components/task/task-list-view.controller.js
+++ src/main/resources/static/components/task/task-list-view.controller.js
@@ -4,7 +4,7 @@
 
     angular
         .module('application.task')
-        .controller('TaskListViewController', [TaskListViewController]);
+        .controller('taskListViewController', [TaskListViewController]);
 
     function TaskListViewController() {
 

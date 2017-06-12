dbfc0a9fc4bbc6f0aaeabcbbbf8eca809d6f2967
onlythenaive <ilya.gubarev@gmail.com>
2017-06-12 21:33:33 +0300

    commented out code removed

--- src/main/resources/static/components/application.config.js
+++ src/main/resources/static/components/application.config.js
@@ -12,33 +12,4 @@
             $urlMatcherFactoryProvider.strictMode(false);
         }
         ]);
-/*
-        .factory('httpRequestInterceptor', [
-            '$localStorage',
-            'loggerService',
-            httpRequestInterceptor
-        ])
-        .config([
-            '$httpProvider',
-            configureHttpProvider
-        ]);
-
-    function configureHttpProvider($httpProvider) {
-        $httpProvider.interceptors.push('httpRequestInterceptor');
-    }
-
-    function httpRequestInterceptor($localStorage, loggerService) {
-        return {
-            request: function (config) {
-                var authentication = $localStorage.authentication;
-                if (authentication) {
-                    var token = authentication.token.id;
-                    config.headers['T-Challenge-Security-Token'] = token;
-                    loggerService.debug("HTTP interceptor applied token: " + token);
-                }
-                return config;
-            }
-        };
-    }
-*/
 })();

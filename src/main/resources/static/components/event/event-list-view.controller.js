cdbe5aaa8519b7a53c6bc7fe64a17e056b19a1fe
onlythenaive <ilya.gubarev@gmail.com>
2017-06-17 19:07:02 +0300

    event list updated

--- src/main/resources/static/components/event/event-list-view.controller.js
+++ src/main/resources/static/components/event/event-list-view.controller.js
@@ -4,9 +4,34 @@
 
     angular
         .module('application.event')
-        .controller('eventListViewController', [EventListViewController]);
+        .controller('eventListViewController', [
+            '$state',
+            'eventService',
+            EventListViewController
+        ]);
 
-    function EventListViewController() {
+    function EventListViewController($state,
+                                     eventService) {
 
+        var self = this;
+
+        self.$onInit = function () {
+            self.search();
+        };
+
+        self.navigateNewEvent = function () {
+            $state.go('root.authorized.event.new');
+        };
+
+        self.search = function () {
+
+            eventService
+
+                .search({})
+
+                .then(function (searchResult) {
+                    self.events = searchResult.items;
+                });
+        };
     }
 })();
